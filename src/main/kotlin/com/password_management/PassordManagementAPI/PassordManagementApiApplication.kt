7685eaee2a9cc9052f0264daaeafdf2cc78c3fc0
ElielRegiani <elielregiani@gmail.com>
2025-08-14 10:22:29 -0300

    First Commit

---
+++ src/main/kotlin/com/password_management/PassordManagementAPI/PassordManagementApiApplication.kt
@@ -0,0 +1,11 @@
+package com.password_management.PassordManagementAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PassordManagementApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PassordManagementApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/password_management/PassordManagementAPI/PassordManagementApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.password_management.PassordManagementAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PassordManagementApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
